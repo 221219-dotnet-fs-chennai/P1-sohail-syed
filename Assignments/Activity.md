02093e2040ed101a9a378a69603b18c62f9b98a4
solusyed <solusyed456@gmail.com>
2022-12-21 09:54:04 +0530

    Assignments was added

---
+++ Assignments/Activity.md
@@ -0,0 +1,9 @@
+pwd
+mkdir Assignments
+cd Assignments
+touch Activity.sh
+touch Activity.md
+vi Activity.sh
+echo 
+read -p
+echo -e
---
+++ Assignments/Activity.sh
@@ -0,0 +1,6 @@
+echo "welcome user!"
+read -p "Enter your first name:" fname
+read -p "Enter your last name:"  lname
+read -p "Enter your DOB:"        DOB
+echo -e "\n$fname $lname"
+echo -e "\n$DOB"
